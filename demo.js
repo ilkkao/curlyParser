3f7e8e1583e973ecc760549a392f351da5f1afac
Ilkka Oksanen <iao@iki.fi>
2014-11-27 17:19:49 +0100

    Simplify demo code

--- demo.js
+++ demo.js
@@ -1,40 +1,37 @@
 
-$(document).ready(main);
-
-function main() {
+$(document).ready(function main() {
     $("#input").on('change keyup paste', function() {
         scan(getInput());
     });
 
     scan(getInput());
-}
+});
 
 var parser = new CurlyParser();
 
 function scan(input) {
-    var output = [];
-
     var start = new Date().getTime();
 
     // Heavy lifting
     var treeAndErrors = parser.parse(input);
 
-    var end = new Date().getTime();
-
-    var time = end - start;
-    $('#time').text('Execution time was ' + time + ' ms.');
-
-    // All done. As an example, convert AST to plain JSON tree
     var ASTTree = treeAndErrors.tree;
     var errors = treeAndErrors.errors;
 
+    var end = new Date().getTime();
+
+    $('#time').text('Execution time was ' + (end - start) + ' ms.');
+
+    // All done. As an example, convert AST to plain JSON tree
     ASTTree.traverse(function(node) {
         delete node.parent; // JSON.stingify below can't handle circles
     });
 
     $('#output').empty().text(JSON.stringify(ASTTree, undefined, 4));
+    printErrors(errors);
+}
 
-    // Print possible syntax errors
+function printErrors(errors) {
     var errorText = '';
 
     errors.forEach(function(error, index) {
@@ -46,8 +43,4 @@ function scan(input) {
 
 function getInput() {
     return $('#input').val();
-}
-
-function logError(text, output) {
-    output.push({ type: 'error', value: text });
 }
